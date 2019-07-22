d645bea7e69c08ca0f031235195ae403c8064661
qizhicheng <qizhicheng@douban.com>
2019-07-22 16:10:00 +0800

    style: function rename and some simplify

--- app/views/api/auth.py
+++ app/views/api/auth.py
@@ -11,17 +11,14 @@ logger = logging.getLogger(__name__)
 
 @bp.route('/auth/login', methods=['POST'])
 @jsonize
-async def challenge(request):
+async def login(request):
     token, msg = await authenticate(request)
-
-    if token:
-        return {'success': True, 'msg': msg, 'token': token}
-    return {'success': False, 'msg': msg, 'token': ''}
+    return {'success': bool(token), 'msg' :msg, 'token': token}
 
 
 @bp.route('/auth/logout', methods=['POST'])
 @requires(['authenticated'])
 @jsonize
-async def revoke(request):
+async def logout(request):
     unauth(request)
     return {'success': True, 'msg': ''}
