bbb6c6b898d4f44c568941cc2b70c25cb563fec7
qizhicheng <qizhicheng@douban.com>
2019-08-13 18:32:41 +0800

    style: change from a-input to a-textarea

--- frontend/src/components/FormWidgets/TextInput.vue
+++ frontend/src/components/FormWidgets/TextInput.vue
@@ -5,18 +5,21 @@
     :label-col="{ span: 5 }"
     :wrapper-col="{ span: 12 }"
   >
-    <a-input
+    <a-textarea
       :name="name"
       :value="value"
       @input="$emit('input',$event.target.value)"
       :placeholder="placeholder"
       v-decorator="decorator"
+      autosize
     />
   </a-form-item>
 </template>
 <script>
+import ATextarea from 'ant-design-vue/es/input/TextArea'
 export default {
   name: 'TextInput',
+  components: {ATextarea},
   props: ['placeholder', 'label', 'name', 'value', 'required', 'extra'],
   computed: {
     decorator () {
