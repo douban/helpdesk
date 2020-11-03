355a2796bf889ea10a3421ce3f7fb1dfb19aeee4
wangqiang <wangqiang@douban.com>
2020-11-03 17:45:44 +0800

    Fix subtab componet modal render error

--- frontend/src/components/SubTabNoRecursive.vue
+++ frontend/src/components/SubTabNoRecursive.vue
@@ -1,19 +1,20 @@
 <template>
-  <a-tabs type="card" :tabPosition="'left'" :activeKey="resultActiveKey" @tabClick='tabClickHandler'>
-    <a-tab-pane v-for="value of resultData.tasks" :key="value.id">
-      <template slot="tab">
-        {{ value.name.length > 30 ? value.name.slice(0, 30) + '...': value.name }}
-      </template>
-      <result-host-table v-bind:data-loaded="dataLoaded" v-bind:resultData="value.result" 
-      :ticketId="ticketId" @showPrettyLog="showPrettyLog"></result-host-table>
+  <div>
+    <a-modal :visible="prettyLogVisible" title="log" ok-text="confirm" cancel-text="cancel" @ok="onConfirm" @cancel="onCancel" width="85%" id="prettyLogModal">
+      <result-host-table v-bind:data-loaded="prettyLogLoaded" v-bind:resultData="prettyLogResult" 
+          :ticketId="ticketId"></result-host-table>
+    </a-modal>
 
-      <a-modal v-model="prettyLogVisible" title="log" ok-text="confirm" cancel-text="cancel" @ok="onConfirm" @cancel="onCancel" width="85%">
-        <result-host-table v-bind:data-loaded="prettyLogLoaded" v-bind:resultData="prettyLogResult" 
-            :ticketId="ticketId"></result-host-table>
-      </a-modal>
-
-    </a-tab-pane>
-  </a-tabs>
+    <a-tabs type="card" :tabPosition="'left'" :activeKey="resultActiveKey" @tabClick='tabClickHandler'>
+      <a-tab-pane v-for="value of resultData.tasks" :key="value.id">
+        <template slot="tab">
+          {{ value.name.length > 30 ? value.name.slice(0, 30) + '...': value.name }}
+        </template>
+        <result-host-table v-bind:data-loaded="dataLoaded" v-bind:resultData="value.result" 
+        :ticketId="ticketId" @showPrettyLog="showPrettyLog"></result-host-table>
+      </a-tab-pane>
+    </a-tabs>
+  </div>
 </template>
 <script>
 import ResultHostTable from './ResultHostTable'
