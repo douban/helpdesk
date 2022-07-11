125687a20e88ab2b299fcb72ff3cc1ef5d825ca6
QSummerY <1272750872@qq.com>
2022-07-11 18:12:21 +0800

    fix create and update

--- frontend/pages/policy/_id/index.vue
+++ frontend/pages/policy/_id/index.vue
@@ -1,55 +1,60 @@
 <template>
   <a-layout>
     <a-card>
-      <router-link :to="{name: 'policy'}">
+      <router-link :to="{ name: 'policy' }">
         <a-icon type="left" /> <span>Return to list</span>
       </router-link>
-      <a-card :title="policyInfo.name" :style="{ marginTop: '16px' }">
-        <a-steps :space="100" >
-          <a-step title="Start"  style="margin-top:24px;margin-bottom:24px"></a-step>
-          <a-step v-for="(node, index) in nodesInfo" :key="index" :title="node.name" :description="node.approvers" style="margin-top:24px;margin-bottom:24px"></a-step>
+      <a-card title="Approval Flow" :style="{ marginTop: '16px' }">
+        <a-steps :space="100">
+          <a-step title="Start" style="margin-top:24px;margin-bottom:24px"></a-step>
+          <a-step v-for="(node, index) in nodesInfo" :key="index" :title="node.name" :description="node.approvers"
+            style="margin-top:24px;margin-bottom:24px"></a-step>
           <a-step title="End" style="margin-top:24px;margin-bottom:24px"></a-step>
-		    </a-steps>
+        </a-steps>
         <a-form layout="inline">
-        	<div>
-          <a-col :span="12" style="margin-bottom: 24px">
-          <a-form-item label="Flow Name">
-            <a-input v-model="policyInfo.name" placeholder="input approver flow name" style="width:300px"></a-input>
-          </a-form-item>
-          </a-col>
-          <a-col :span="12" style="margin-bottom: 24px">
-          <a-form-item label="Description">
-            <a-input v-model="policyInfo.display" placeholder="input description" style="width:300px"></a-input>
-          </a-form-item>
-          </a-col>
+          <div>
+            <a-col :span="12" style="margin-bottom: 24px">
+              <a-form-item label="Flow Name">
+                <a-input v-model="policyInfo.name" placeholder="input approver flow name" style="width:300px"></a-input>
+              </a-form-item>
+            </a-col>
+            <a-col :span="12" style="margin-bottom: 24px">
+              <a-form-item label="Description">
+                <a-input v-model="policyInfo.display" placeholder="input description" style="width:300px"></a-input>
+              </a-form-item>
+            </a-col>
           </div>
         </a-form>
+
         <a-divider>Nodes</a-divider>
-        <a-form v-for="(node, index) in nodesInfo" 
-          :key="index" 
-          style="margin-bottom: 24px" 
-          layout="inline">
+
+        <a-form v-for="(node, index) in nodesInfo" :key="index"
+          style="margin-bottom: 24px" layout="inline" @change="changeInput">
           <a-form-item label="name" name="name">
-	 	        <a-input v-model="node.name" placeholder="input node name"></a-input>
- 	        </a-form-item>
+            <a-input v-model="node.name" placeholder="input node name" :rules="{
+              required: true,
+              message: 'node name can not be null',
+              trigger: 'change',
+            }"></a-input>
+          </a-form-item>
           <a-form-item label="approvers" name="approvers">
-	 	        <a-input v-model="node.approvers" placeholder="input node approvers"></a-input>
- 	        </a-form-item>
+            <a-input v-model="node.approvers" placeholder="input node approvers"></a-input>
+          </a-form-item>
           <a-form-item label="desc" name="desc">
-	 	        <a-input v-model="node.desc" placeholder="input node description"></a-input>
- 	        </a-form-item>
+            <a-input v-model="node.desc" placeholder="input node description"></a-input>
+          </a-form-item>
           <a-form-item label="next" name="next">
-	 	        <a-input v-model="node.next" placeholder="input next node name"></a-input>
- 	        </a-form-item>
+            <a-input v-model="node.next" placeholder="input next node name"></a-input>
+          </a-form-item>
           <a-form-item>
-		        <a-icon type="plus-circle" @click="addNode"/>
- 	        </a-form-item>
- 	        <a-form-item v-if="index !== 0">
-		        <a-icon type="minus-circle" @click="removeNode(node)"/>
- 	        </a-form-item>
+            <a-icon type="plus-circle" @click="addNode" />
+          </a-form-item>
+          <a-form-item v-if="index !== 0">
+            <a-icon type="minus-circle" @click="removeNode(node)" />
+          </a-form-item>
         </a-form>
-         <div style="text-align: center; margin-top: 32px">
-            <a-button type="primary" :disabled="!canSubmit" @click="handleSubmit">Submit</a-button>
+        <div style="text-align: center; margin-top: 32px">
+          <a-button type="primary" :disabled="!canSubmit" @click="handleSubmit">Submit</a-button>
         </div>
       </a-card>
     </a-card>
@@ -57,12 +62,14 @@
 </template>
 
 <script>
-import {UTCtoLcocalTime} from '@/utils/HDate'
+import { UTCtoLcocalTime } from '@/utils/HDate'
 export default {
-  data () {
+  data() {
     return {
       table_data: [{}],
       filtered: {},
+      policyInfo: {},
+      nodesInfo: [{}],
       loadingIntervalId: null,
       canSubmit: true,
       autoRefreshOn: false,
@@ -73,28 +80,13 @@ export default {
     }
   },
   computed: {
-    policyInfo () {
-      if (this.table_data) {
-        return this.table_data[0]
-      }
-      return {}
-    },
-    nodesInfo () {
-      const nodes = "nodes"
-      if (this.policyInfo.definition) {
-        if (this.policyInfo.definition[nodes]) {
-          return this.policyInfo.definition[nodes]
-        }
-      }
-      return [{}]
-    }
   },
   watch: {
-    '$route' () {
+    '$route'() {
       this.loadPolicy()
     }
   },
-  mounted () {
+  mounted() {
     this.loadPolicy()
   },
   methods: {
@@ -103,19 +95,28 @@ export default {
       this.$axios.get('/api/policies/' + this.$route.params.id).then(
         (response) => {
           this.table_data = response.data.policies
+          if (this.table_data[0]) {
+            this.policyInfo = this.table_data[0]
+          }
+          if (this.table_data[0?.definition?.nodes])  {
+              this.nodesInfo = this.policyInfo.definition.nodes
+          }
         })
     },
-	  addNode() { 
-		  this.nodesInfo.push({})
+    addNode() {
+      this.nodesInfo.push({})
     },
     removeNode(node) {
-		  const index = this.nodesInfo.indexOf(node);
-		  if(index !== -1) {
-			  this.nodesInfo.splice(index, 1);
-		  }
-	  },
-    handleSubmit () {
-      const data = {"name": this.policyInfo.name, "display": this.policyInfo.display, "definition": {nodes: this.nodesInfo}}
+      const index = this.nodesInfo.indexOf(node);
+      if (index !== -1) {
+        this.nodesInfo.splice(index, 1);
+      }
+    },
+    changeInput() {
+      this.$forceUpdate()
+    },
+    handleSubmit() {
+      const data = { "name": this.policyInfo.name, "display": this.policyInfo.display, "definition": { nodes: this.nodesInfo } }
       this.$axios.post('/api/policies/' + this.$route.params.id, data).then(
         (response) => {
           this.table_data = response.data.policies
@@ -130,7 +131,7 @@ export default {
 </script>
 
 <style scoped>
-  .whiteBackground {
-    background: #fff
-  }
+.whiteBackground {
+  background: #fff
+}
 </style>
--- frontend/pages/policy/index.vue
+++ frontend/pages/policy/index.vue
@@ -15,9 +15,6 @@
       <span slot="action" slot-scope="text, record">
         <NuxtLink :to="{name: 'policy-id', params: {id: record.id}}">detail</NuxtLink>
         <a-divider type="vertical" />
-        <!-- <h-associate-drawer v-if="$store.getters.isAdmin"></h-associate-drawer> -->
-        <NuxtLink :to="{name: 'policy-id', params: {id: record.id}}">associate</NuxtLink>
-        <a-divider type="vertical" />
         <a-popconfirm title="Sure to delete?" ok-text="Ok" cancel-text="Cancel" 
           @confirm="delPolicy(record.id)">
           <a>delete</a>
@@ -32,113 +29,110 @@ import {cmp} from '@/utils/HComparer'
 import {UTCtoLcocalTime} from '@/utils/HDate'
 
 export default {
-  name: 'HPolicyList',
-  data () {
-    return {
-      tableData: [],
-      filtered: {},
-      loading: false,
-      pagination: {
-        pageSize: 1
-      },
-    }
-  },
-  computed: {
-    columns () {
-      return [{
-        title: 'ID',
-        key: 'id',
-        dataIndex: 'id',
-        sorter: (a, b) => cmp(a, b, 'id'),
-        scopedSlots: { customRender: 'id' }
-      }, {
-        title: 'Name',
-        key: 'name',
-        dataIndex: 'name',
-        sorter: (a, b) => cmp(a, b, 'name')
-      }, {
-        title: 'Display',
-        key: 'display',
-        width: 150,
-        dataIndex: 'display',
-        sorter: (a, b) => cmp(a, b, 'display'),
-        scopedSlots: { customRender: 'display' }
-      }, {
-        title: 'By',
-        key: 'created_by',
-        dataIndex: 'created_by',
-        sorter: (a, b) => cmp(a, b, 'created_by')
-      }, {
-        title: 'Create Time',
-        key: 'created_at',
-        dataIndex: 'created_at',
-        sorter: (a, b) => cmp(a, b, 'created_at'),
-        customRender: (text) => {
-          return UTCtoLcocalTime(text)
-        }
-      }, {
-        title: 'Update by',
-        key: 'updated_by',
-        dataIndex: 'updated_by',
-        sorter: (a, b) => cmp(a, b, 'updated_by')
-      }, {
-        title: 'Update Time',
-        key: 'updated_at',
-        dataIndex: 'updated_at',
-        sorter: (a, b) => cmp(a, b, 'updated_at'),
-        customRender: (text) => {
-          return UTCtoLcocalTime(text)
-        }
-      }, {
-        title: 'Action',
-        key: 'action',
-        width: 230,
-        scopedSlots: { customRender: 'action' }
-      }]
-    }
-  },
-  mounted () {
-    const queryParams = this.$route.query
-    if (queryParams.page === undefined) {
-      queryParams.page = 1
-    }
-    if (queryParams.pagesize === undefined) {
-      queryParams.pagesize = 10
-    }
-    queryParams.page = Number(queryParams.page)
-    queryParams.pageSize = Number(queryParams.pageSize || 10)
-    this.loadPolicies(queryParams)
-  },
-  methods: {
-    loadPolicies (params) {
-      this.pagination.current = params.page
-      this.loading = true
-      this.$axios.get('/api/policies', {params}).then(
-        (response) => {
-          this.handlePolicyList(response)
-        }
-      )
-    },    
-    handlePolicyList (response) {
-      this.pagination.total = response.data.total
-      this.pagination.current = response.data.page
-      this.pagination.pageSize = response.data.page_size || 20
-      // pagination.pageSize  and pagination.current are decorated by ```.sync``
-      // the following line is vital. dont know why, just do it.
-      this.pagination = {...this.pagination}
-      this.tableData = response.data.items
-      this.loading = false
+    name: "HPolicyList",
+    data() {
+        return {
+            tableData: [],
+            filtered: {},
+            loading: false,
+            pagination: {
+                pageSize: 1
+            },
+        };
     },
-    delPolicy (id) {
-      this.$axios.delete('/api/policies/' + id).then(
-        res => { 
-          this.$message.info("success!")
-      }).catch((error) => {
-        this.$message.warning(error)
-        })
-      this.loadPolicies(this.$route.params)
+    computed: {
+        columns() {
+            return [{
+                    title: "ID",
+                    key: "id",
+                    dataIndex: "id",
+                    sorter: (a, b) => cmp(a, b, "id"),
+                    scopedSlots: { customRender: "id" }
+                }, {
+                    title: "Name",
+                    key: "name",
+                    dataIndex: "name",
+                    sorter: (a, b) => cmp(a, b, "name")
+                }, {
+                    title: "Display",
+                    key: "display",
+                    width: 150,
+                    dataIndex: "display",
+                    sorter: (a, b) => cmp(a, b, "display"),
+                    scopedSlots: { customRender: "display" }
+                }, {
+                    title: "By",
+                    key: "created_by",
+                    dataIndex: "created_by",
+                    sorter: (a, b) => cmp(a, b, "created_by")
+                }, {
+                    title: "Create Time",
+                    key: "created_at",
+                    dataIndex: "created_at",
+                    sorter: (a, b) => cmp(a, b, "created_at"),
+                    customRender: (text) => {
+                        return UTCtoLcocalTime(text);
+                    }
+                }, {
+                    title: "Update by",
+                    key: "updated_by",
+                    dataIndex: "updated_by",
+                    sorter: (a, b) => cmp(a, b, "updated_by")
+                }, {
+                    title: "Update Time",
+                    key: "updated_at",
+                    dataIndex: "updated_at",
+                    sorter: (a, b) => cmp(a, b, "updated_at"),
+                    customRender: (text) => {
+                        return UTCtoLcocalTime(text);
+                    }
+                }, {
+                    title: "Action",
+                    key: "action",
+                    width: 230,
+                    scopedSlots: { customRender: "action" }
+                }];
+        }
     },
-  }
+    mounted() {
+        const queryParams = this.$route.query;
+        if (queryParams.page === undefined) {
+            queryParams.page = 1;
+        }
+        if (queryParams.pagesize === undefined) {
+            queryParams.pagesize = 10;
+        }
+        queryParams.page = Number(queryParams.page);
+        queryParams.pageSize = Number(queryParams.pageSize || 10);
+        this.loadPolicies(queryParams);
+    },
+    methods: {
+        loadPolicies(params) {
+            this.pagination.current = params.page;
+            this.loading = true;
+            this.$axios.get("/api/policies", { params }).then((response) => {
+                this.handlePolicyList(response);
+            });
+        },
+        handlePolicyList(response) {
+            this.pagination.total = response.data.total;
+            this.pagination.current = response.data.page;
+            this.pagination.pageSize = response.data.page_size || 20;
+            // pagination.pageSize  and pagination.current are decorated by ```.sync``
+            // the following line is vital. dont know why, just do it.
+            this.pagination = { ...this.pagination };
+            this.tableData = response.data.items;
+            this.loading = false;
+        },
+        delPolicy(id) {
+            this.$axios.delete("/api/policies/" + id).then(res => {
+                this.$message.info("success!");
+            }).catch((error) => {
+                this.$message.warning(error);
+            });
+            this.loadPolicies(this.$route.params);
+        },
+    },
 }
 </script>
 
