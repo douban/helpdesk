b4208787c2d1fd445ae12d16485a997e92d7bb9d
QSummerY <1272750872@qq.com>
2022-08-22 16:15:21 +0800

    get rid of contrib changes

--- contrib/docker/prestart.sh
+++ contrib/docker/prestart.sh
@@ -4,9 +4,6 @@ cd /app
 if [ -f /app/secret/local_config.py ]; then
     cp /app/secret/local_config.py /app/local_config.py
 fi
-if [ -f /app/secret/bridge.py ]; then
-    cp /app/secret/bridge.py /app/bridge.py
-fi
 
 # TODO: init database
 # make database
