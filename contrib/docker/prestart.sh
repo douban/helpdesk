9d734f771d82fcc1e63494ea3b4366ebd526a9ff
QSummerY <1272750872@qq.com>
2022-08-22 15:15:27 +0800

    update contrib

--- contrib/docker/prestart.sh
+++ contrib/docker/prestart.sh
@@ -4,6 +4,9 @@ cd /app
 if [ -f /app/secret/local_config.py ]; then
     cp /app/secret/local_config.py /app/local_config.py
 fi
+if [ -f /app/secret/bridge.py ]; then
+    cp /app/secret/bridge.py /app/bridge.py
+fi
 
 # TODO: init database
 # make database
